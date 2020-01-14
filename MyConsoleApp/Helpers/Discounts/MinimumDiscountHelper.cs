47762d38c5b393e01af009895dbdec25fc39eeab
Whisnu Sucitanuary <whisnusucitanuary@gmail.com>
2020-01-14 17:38:09 +0800

    Finished solution

--- MyConsoleApp/Helpers/Discounts/MinimumDiscountHelper.cs
+++ MyConsoleApp/Helpers/Discounts/MinimumDiscountHelper.cs
@@ -9,7 +9,22 @@ namespace MyConsoleApp.Helpers.Discounts
 	{
 		public decimal ApplyDiscount(decimal total)
 		{
-			throw new NotImplementedException();
+			if (total < 0)
+			{
+				throw new ArgumentOutOfRangeException();
+			}
+			else if (total > 100)
+			{
+				return total * 0.9M;
+			}
+			else if (total >= 10 && total <= 100)
+			{
+				return total - 5;
+			}
+			else
+			{
+				return total;
+			}
 		}
 	}
 }
